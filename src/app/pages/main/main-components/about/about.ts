23c592975cf13e1d372f7cbdd5a37aa72d4dd3a6
Anika Mayer <mayer.anika@protonmail.com>
2026-09-25 14:55:22 +0200

    style: basic styling about-section

--- src/app/pages/main/main-components/about/about.ts
+++ src/app/pages/main/main-components/about/about.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { Button } from '../../../../shared/components/button/button';
 
 @Component({
-  imports: [],
+  imports: [Button],
   selector: 'app-about',
   styleUrl: './about.scss',
   templateUrl: './about.html',
